1b70454ed5125078f307a3e4148eb9edec5cf611
satyaa232323 <attalasatya@gmail.com>
2025-01-30 22:35:25 +0700

    Fix import statement casing for Hero component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import Hero from './components/hero'
 import About from './components/About'
 import MySkills from './components/MySkills'
 import Accordion from './components/Accordion';
